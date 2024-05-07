8e3c39768f572d95cd9d3bfdefa8c19ea1f01180
kiddogit <85332892+kiddogit@users.noreply.github.com>
2024-05-07 20:11:38 +0545

    fixed styling

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -16,7 +16,7 @@ const RegisterPage = () => {
     <div className='h-screen w-full flex flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold font-merri'>Create an account</h1>
 
-      <form className='mt-10 w-[400px]'>
+      <form className='mt-11 w-[400px]'>
         <input type='text' className='w-full p-2 bg-white border outline-none' />
       </form>
     </div>
